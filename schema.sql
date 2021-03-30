50f51fc67b647876d498634b2a12fe1b65e36d87
marykristina4 <mary.ehrstedt@helsinki.fi>
2021-03-30 15:35:58 +0300

    schemaa päivitetty

--- schema.sql
+++ schema.sql
@@ -1,1 +1,6 @@
 CREATE TABLE visitors (id SERIAL PRIMARY KEY, time TIMESTAMP);
+CREATE TABLE statuses (id SERIAL PRIMARY KEY, value INTEGER, description TEXT);
+CREATE TABLE wages (id SERIAL PRIMARY KEY, description TEXT, hour_wage INTEGER);
+CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT, role INTEGER, wage_id INTEGER REFERENCES wages);
+CREATE TABLE chores (id SERIAL PRIMARY KEY, description TEXT, responsible_id INTEGER REFERENCES users, hours INTEGER, status_id INTEG
+ER REFERENCES statuses);
