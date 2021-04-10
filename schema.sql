148a1668b7562bf0e48d6246f7f0ccd46e6db8ca
marykristina4 <mary.ehrstedt@helsinki.fi>
2021-04-10 12:15:01 +0300

    schema korjattu

--- schema.sql
+++ schema.sql
@@ -3,5 +3,4 @@ CREATE TABLE statuses (id SERIAL PRIMARY KEY, value INTEGER, description TEXT);
 CREATE TABLE wages (id SERIAL PRIMARY KEY, description TEXT, hour_wage INTEGER);
 CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT, role INTEGER, wage_id INTEGER REFERENCES wages);
 CREATE TABLE categories (id SERIAL PRIMARY KEY, value INTEGER, description TEXT);
-CREATE TABLE chores (id SERIAL PRIMARY KEY, description TEXT, responsible_id INTEGER REFERENCES users, minutes INTEGER, status_id INTEG
-ER REFERENCES statuses, category_id INTEGER REFERENCES categories);
+CREATE TABLE chores (id SERIAL PRIMARY KEY, description TEXT, responsible_id INTEGER REFERENCES users, minutes INTEGER, status_id INTEGER REFERENCES statuses, category_id INTEGER REFERENCES categories);
